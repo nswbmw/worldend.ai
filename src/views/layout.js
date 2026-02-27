572ee010febcc0b922917adcf6b92ec5fe3bfed4
RosApr <287134285@qq.com>
2026-02-27 14:55:59 +0800

    refactor: use bundle styles

--- src/views/layout.js
+++ src/views/layout.js
@@ -5,7 +5,7 @@ const layout = `<!DOCTYPE html>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
   <meta name="color-scheme" content="dark light">
   <script>
-    (function(){
+    (function() {
       try {
         const theme = localStorage.getItem('wd_darkMode');
         let effectiveTheme;
@@ -25,10 +25,8 @@ const layout = `<!DOCTYPE html>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/3.13.3/cdn.min.js" defer></script>
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link rel="stylesheet" href="/styles/base.css" />
   <link rel="stylesheet" href="/styles/layout.css" />
   <link href="https://fonts.googleapis.com/css2?family=Geist+Mono:wght@400;500&family=Instrument+Serif:ital@0;1&family=DM+Sans:wght@300;400;500;600&display=swap" rel="stylesheet" />
-  <script src="https://cdn.tailwindcss.com/3.4.17"></script>
   {{> googleAnalysis}}
 </head>
 <body x-data="App()" x-init="init()">
