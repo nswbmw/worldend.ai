ad0e9ab0dd2905e88694351cf21a1e9b924f39eb
RosApr <287134285@qq.com>
2026-04-27 22:11:57 +0800

    fix: use ISO 8601 format for RSS and sitemap dates

--- src/route/sitemap.js
+++ src/route/sitemap.js
@@ -7,24 +7,25 @@ export default function route (app) {
   app.get(
     '/sitemap.xml',
     async (ctx, next) => {
-      const date = dayjs().format('YYYY-MM-DD')
+      const date = dayjs().toISOString()
 
       const prophecies = await getProphecySlugsWithCache()
 
       ctx.res.type = 'application/xml'
       ctx.res.body = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
-  <url>
-    <loc>${config.webHost}/</loc>
-    <lastmod>${date}</lastmod>
-    <changefreq>daily</changefreq>
-    <priority>1.0</priority>
-  </url>
+          <url>
+            <loc>${config.webHost}/</loc>
+            <lastmod>${date}</lastmod>
+            <changefreq>daily</changefreq>
+            <priority>1.0</priority>
+          </url>
 
-  ${prophecies.map(generateLoc).join('\n')}
+          ${prophecies.map(generateLoc).join('\n')}
 
-</urlset>`
+        </urlset>
+      `
     }
   )
 }
@@ -32,7 +33,7 @@ export default function route (app) {
 function generateLoc (prophecy) {
   return `<url>
     <loc>${config.webHost}/en/${prophecy.slug}</loc>
-    <lastmod>${prophecy.date}</lastmod>
+    <lastmod>${prophecy.date}T00:00:00Z</lastmod>
     <changefreq>never</changefreq>
     <priority>0.9</priority>
   </url>`
